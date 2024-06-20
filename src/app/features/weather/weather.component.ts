b71b402dad958cb25ef2b0d75a376013c464912f
Will-h0219 <wherrera0219@gmail.com>
2024-06-19 20:42:25 -0500

    found a better design, restarting the project

--- src/app/features/weather/weather.component.ts
+++ src/app/features/weather/weather.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { LeftSideComponent } from './components/left-side/left-side.component';
-import { WeatherContentComponent } from './components/weather-content/weather-content.component';
 
 @Component({
   selector: 'app-weather',
   standalone: true,
   imports: [
-    LeftSideComponent,
-    WeatherContentComponent
   ],
   templateUrl: './weather.component.html',
   styleUrl: './weather.component.scss'
