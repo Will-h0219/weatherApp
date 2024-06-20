5dcb5bb3f1316129aed095e5a2edfc135dfb7a4d
Will-h0219 <wherrera0219@gmail.com>
2024-06-19 21:49:45 -0500

    basic layout, change material theme

--- src/app/features/weather/weather.component.ts
+++ src/app/features/weather/weather.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { GeneralInfoComponent } from './components/general-info/general-info.component';
+import { WeatherDetailComponent } from './components/weather-detail/weather-detail.component';
+import { SearchBarComponent } from '../../common/search-bar/search-bar.component';
 
 @Component({
   selector: 'app-weather',
   standalone: true,
   imports: [
+    GeneralInfoComponent,
+    SearchBarComponent,
+    WeatherDetailComponent
   ],
   templateUrl: './weather.component.html',
   styleUrl: './weather.component.scss'
