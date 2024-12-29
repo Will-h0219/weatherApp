ba7755b521053c370e9365dbd6c8b024f406bc9e
William Herrera <wherrera0219@gmail.com>
2024-12-29 13:33:26 -0500

    add preliminal components before angular update

--- src/app/features/weather/components/header/header.component.ts
+++ src/app/features/weather/components/header/header.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { ThemeToggleComponent } from '../../../../common/theme-toggle/theme-toggle.component';
+import { SearchBarComponent } from '../../../../common/search-bar/search-bar.component';
+import { CustomButtonComponent } from '../../../../common/custom-button/custom-button.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
+    CustomButtonComponent,
+    SearchBarComponent,
     ThemeToggleComponent,
   ],
   templateUrl: './header.component.html',
