0cd39c2a8c96bf29bec68b01017ad9d851840c49
William Herrera <wherrera0219@gmail.com>
2024-12-29 13:38:15 -0500

    update to angular 18

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { HttpClientModule } from '@angular/common/http';
+import {} from '@angular/common/http';
 
 import { routes } from './app.routes';
 
