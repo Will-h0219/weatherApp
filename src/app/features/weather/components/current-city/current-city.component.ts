f2515bcd38058651757b192a41fcd389ce569267
William Herrera <wherrera0219@gmail.com>
2024-12-30 22:45:53 -0500

    integrated signal data with template-current city

--- src/app/features/weather/components/current-city/current-city.component.ts
+++ src/app/features/weather/components/current-city/current-city.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, input, Input } from '@angular/core';
 import { CardComponent } from '../../../../common/card/card.component';
 import { Location } from '../../../../data/interfaces/location.interface';
 import { CommonModule } from '@angular/common';
@@ -13,5 +13,5 @@ import { CommonModule } from '@angular/common';
   styleUrl: './current-city.component.scss'
 })
 export class CurrentCityComponent {
-  @Input() locationData?: Location;
+  location = input<Location>();
 }
